c4a8ed11da9d173beed37c23f48237fe137e0261
Matias Lindgren <matias.lindgren@iki.fi>
2026-03-28 13:52:40 -0400

    cleanup, smoke-test hasher import

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,3 @@
-def test_import_empty():
-    import empty  # noqa: F401
-
-
 def test_import_hello_lib():
     import hello_lib  # noqa: F401
     from hello_lib import say_hello_to  # noqa: F401
@@ -10,3 +6,9 @@ def test_import_hello_lib():
 def test_import_is_prime():
     import is_prime  # noqa: F401
     from is_prime import is_prime  # noqa: F401,F811
+
+
+def test_import_hasher():
+    import hasher  # noqa: F401
+    from hasher import djb2  # noqa: F401
+    from hasher import strings_pyhash  # noqa: F401
