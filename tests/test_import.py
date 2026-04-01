54e949d3d5c0b4e740d40ee277a58f8763d761da
Matias Lindgren <matias.lindgren@iki.fi>
2026-03-31 22:43:09 -0400

    test ext modules are imported as shared obj

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,14 +1,23 @@
 def test_import_hello_lib():
     import hello_lib  # noqa: F401
+
+    assert hello_lib.__file__.endswith(".so")
+
     from hello_lib import say_hello_to  # noqa: F401
 
 
 def test_import_is_prime():
     import is_prime  # noqa: F401
+
+    assert is_prime.__file__.endswith(".so")
+
     from is_prime import is_prime  # noqa: F401,F811
 
 
 def test_import_hasher():
     import hasher  # noqa: F401
+
+    assert hasher.__file__.endswith(".so")
+
     from hasher import djb2  # noqa: F401
     from hasher import strings_pyhash  # noqa: F401
